fd7f7e98b29ecb8519638d932892d1541a48fc04
KbatQuestion <99158684+KbatQuestion@users.noreply.github.com>
2023-12-20 12:46:49 +0800

    Gui Semi Done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProjectTrifecta
 🚀 Welcome to ProjectTrifecta, where coding, styling, and undeniable charm converge in a symphony of web development magic! 🎩✨
+
+
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function createNewInput() {
     dynamicInputContainer.innerHTML = '';
 
     // Check if the selected option requires a dynamic input
-    if (selectedOption === "Non Preemptive Priority" || selectedOption === "Preemptive Priority") {
+    if (selectedOption === "Non Preemptive Priority" || selectedOption === "Preemptive Priority" || selectedOption == "Round Robin with Quantum 3") {
 
 
         var newLabel = document.createElement("label");
@@ -27,3 +27,46 @@ function createNewInput() {
         
     }
 }
+
+
+var groupCount = 1;
+var previousBoxendNumber;
+
+function processButton(){
+ 
+
+      // Create a new box element
+      var newBox = document.createElement("div");
+      newBox.className = "box";
+      newBox.textContent = "P" + groupCount;
+
+      // Create a new box number element
+      var boxNumber = document.createElement("div");
+      boxNumber.className = "box-number";
+      boxNumber.textContent = groupCount;
+
+      // Remove the previous boxendNumber if exists
+      if (previousBoxendNumber && previousBoxendNumber.parentNode) {
+          previousBoxendNumber.parentNode.removeChild(previousBoxendNumber);
+      }
+
+      // Create a new boxendNumber element
+      var boxendNumber = document.createElement("div");
+      boxendNumber.className = "box_endnumber";
+      boxendNumber.textContent = groupCount + 1;
+
+      // Set the current boxendNumber as the previous for the next iteration
+      previousBoxendNumber = boxendNumber;
+
+      // Increment the groupCount for the next group
+      groupCount++;
+
+      // Create a new container for the box and its number
+      var boxContainer = document.createElement("div");
+      boxContainer.appendChild(newBox);
+      boxContainer.appendChild(boxNumber);
+      boxContainer.appendChild(boxendNumber);
+
+      // Append the new container to the main container
+      document.getElementById("container2").appendChild(boxContainer);
+  }
